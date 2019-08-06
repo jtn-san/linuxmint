39122936f4bfdf0627ef7274ec0a619fab71616f
Robson Vaamonde <robsonvaamonde@gmail.com>
2019-08-06 17:09:51 -0300

    Add files via upload

---
+++ comandos.sh
@@ -0,0 +1,43 @@
+    1  ls
+    2  ls -lh
+    3  cd /
+    4  ls
+    5  cd -
+    6  pwd
+    7  mkdir linuxmint
+    8  ls
+    9  cd linuxmint/
+   10  pwd
+   11  cd /
+   12  pwd
+   13  cd -
+   14  sudo apt update && sudo apt install git
+   15  git clone https://github.com/vaamonde/linuxmint.git
+   16  ls
+   17  cd linuxmint/
+   18  ls -lh
+   19  echo 
+   20  echo Perfect K.O
+   21  echo SENAC Tatuapé aprendendo Bash Shell no Linux Mint
+   22  echo SENAC Tatuapé aprendendo Bash Shell no Linux Mint > vaamonde.sh
+   23  ls
+   24  ls -lh
+   25  touch senac.sh
+   26  ls -lh
+   27  cat LICENSE 
+   28  cat -n LICENSE 
+   29  head LICENSE 
+   30  head -n20 LICENSE 
+   31  tail LICENSE 
+   32  tail -n20 LICENSE 
+   33  less LICENSE 
+   34  cat -n LICENSE | tail 
+   35  cat -n LICENSE | head -n200 | tail -n101 | less
+   36  cd linuxmint/linuxmint/
+   37  ls 
+   38  ls -lh
+   39  cat -n LICENSE | head -n200
+   40  cat -n LICENSE | head -n200 | tail -n101
+   41  cat -n LICENSE | head -n200 | tail -n101 | less
+   42  history 
+   43  history > comandos.sh
